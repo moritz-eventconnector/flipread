645686bd7ffa2e5b03dfe17cdeaf374020cbff05
EventConnector <moritz@eventconnector.ch>
2026-01-30 18:02:15 +0100

    repair update

--- apps/backend/config/settings.py
+++ apps/backend/config/settings.py
@@ -46,6 +46,7 @@ MIDDLEWARE = [
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
